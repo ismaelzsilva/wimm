2666eeba3a3e1611572fb764bc078a8b57aaea86
Ismael <38179854+ismaelzsilva@users.noreply.github.com>
2026-06-07 18:11:58 +0200

    add categroy model

--- wallet/models.py
+++ wallet/models.py
@@ -16,6 +16,22 @@ class Wallet(BaseModel):
         return self.name
 
 
+class Category(BaseModel):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="categories")
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "categories"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["owner", "name"], name="unique_category_per_user"
+            )
+        ]
+
+    def __str__(self):
+        return self.name
+
+
 class TransferGroup(BaseModel):
     pass
 
@@ -36,6 +52,13 @@ class Transaction(BaseModel):
     )
     description = models.CharField(max_length=500, blank=True)
     date = models.DateField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="transactions",
+    )
     transfer_group = models.ForeignKey(
         TransferGroup,
         on_delete=models.SET_NULL,
