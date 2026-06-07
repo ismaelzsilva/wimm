a9e1f58b1f9817b6b1a2d38ba7edfefe2f70ca7c
Ismael <38179854+ismaelzsilva@users.noreply.github.com>
2026-06-07 16:11:32 +0200

    use base model in user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 
+from wimm.base_models import BaseModel
 
-class CustomUser(AbstractUser):
+
+class CustomUser(BaseModel, AbstractUser):
     pass
 
     def __str__(self):
